b960716fec8f73deca5372b94bda7d2fef94d5f3
Yuchi Hsueh <bhsueh94@gmail.com>
2022-08-31 12:30:47 -0300

    search bar with pg search

--- app/models/route.rb
+++ app/models/route.rb
@@ -4,4 +4,10 @@ class Route < ApplicationRecord
   belongs_to :user
   has_many :records
   has_many :reservations, through: :records
+  include PgSearch::Model
+  pg_search_scope :search_by_address,
+                  against: [:address],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
