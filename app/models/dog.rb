920cf5e6558046c53f0b74a13ad2c00a69b607c4
Victor Sa <sa.victor.cp@gmail.com>
2022-09-08 13:36:14 -0300

    validation

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,7 +4,7 @@ class Dog < ApplicationRecord
   has_one_attached :photo
   validates :name, presence: true
   validates :breed, presence: true
-  validates :birthday, presence: true, comparison: { greater_than: DateTime.now }
+  validates :birthday, presence: true, comparison: { smaller_than: DateTime.now }
   validates :size, presence: true
   validates :weight, presence: true, length: { in: 1..100 }
 end
