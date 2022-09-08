71471518ecf9e2b99388493c4c1684c2c1045dec
Eduardo Aguilar <laloaps@gmail.com>
2022-09-08 13:05:13 -0400

    age in years

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,7 +4,17 @@ class Dog < ApplicationRecord
   has_one_attached :photo
   validates :name, presence: true
   validates :breed, presence: true
-  validates :birthday, presence: true, comparison: { greater_than: DateTime.now }
+  validates :birthday, presence: true, comparison: { less_than: DateTime.now }
   validates :size, presence: true
   validates :weight, presence: true, length: { in: 1..100 }
+
+  def age
+    duration_in_years.round
+  end
+
+  private
+
+  def duration_in_years
+    (DateTime.now.to_date - birthday) / 365.0
+  end
 end
